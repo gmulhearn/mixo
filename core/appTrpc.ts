0e5895a6613d56af11a6404c4a7f7b6558ce4f97
gmulhearn <gmulhearn@proton.me>
2023-04-09 21:03:06 +1000

    url redir
    Signed-off-by: gmulhearn <gmulhearn@proton.me>

--- core/appTrpc.ts
+++ core/appTrpc.ts
@@ -2,11 +2,16 @@ import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../server/routers/_app';
 
-export function getBaseUrl() {
+export function getBaseUrl(): string {
     if (typeof window !== 'undefined') {
         // browser should use relative path
         return ''
     }
+    if (process.env.BASE_APP_URL) {
+        // manually defined base app url
+        return process.env.BASE_APP_URL
+    }
+
     if (process.env.VERCEL_URL) {
         // reference for vercel.com
         return `https://${process.env.VERCEL_URL}`
