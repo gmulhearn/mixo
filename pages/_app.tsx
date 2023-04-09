073fc5106e677df3de3f8d7977420fe6fef8d3e1
gmulhearn <gmulhearn@proton.me>
2023-04-09 20:54:39 +1000

    try fix color manager
    Signed-off-by: gmulhearn <gmulhearn@proton.me>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { trpc } from '@/core/appTrpc'
+import theme from '@/styles/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps, AppType } from 'next/app'
 
 const App: AppType = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
